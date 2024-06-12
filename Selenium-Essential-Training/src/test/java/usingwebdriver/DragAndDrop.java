ad6178e0292d238087370891bbbdb8412d4a13c6
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-12 16:06:23 -0500

    Drag and drop

--- Selenium-Essential-Training/src/test/java/usingwebdriver/DragAndDrop.java
+++ Selenium-Essential-Training/src/test/java/usingwebdriver/DragAndDrop.java
@@ -1,5 +1,27 @@
 package usingwebdriver;
 
-public class DragAndDrop {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
+public class DragAndDrop {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://formy-project.herokuapp.com/dragdrop");
+
+        WebElement image = driver.findElement(By.id("image"));
+
+        WebElement box = driver.findElement(By.id("box"));
+
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(image, box).build().perform();
+
+        Thread.sleep(1500);
+        driver.quit();
+    }
 }
