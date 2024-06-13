2684fc758ecfe97c3525c701405f9791f75e6a76
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-13 10:03:03 -0500

    Radio Buttons

---
+++ Selenium-Essential-Training/src/test/java/interacting/RadioButtons.java
@@ -0,0 +1,26 @@
+package interacting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class RadioButtons {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://formy-project.herokuapp.com/radiobutton");
+
+        WebElement radioButton1 = driver.findElement(By.id("radio-button-1"));
+        radioButton1.click();
+
+        WebElement radioButton2 = driver.findElement(By.cssSelector("input[value='option2']"));
+        radioButton2.click();
+
+        WebElement radioButton3 = driver.findElement(By.xpath("/html/body/div/div[3]/input"));
+        radioButton3.click();
+        driver.quit();
+    }
+}
