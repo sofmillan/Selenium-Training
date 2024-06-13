beb6d9d6a640f98f2ddfb6f5f08c436672c28e9d
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-13 10:19:25 -0500

    File upload

---
+++ Selenium-Essential-Training/src/test/java/interacting/FileUpload.java
@@ -0,0 +1,21 @@
+package interacting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FileUpload {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://formy-project.herokuapp.com/fileupload");
+
+        WebElement fileUploadField = driver.findElement(By.id("file-upload-field"));
+        fileUploadField.sendKeys("file.png");
+
+        driver.quit();
+    }
+}
