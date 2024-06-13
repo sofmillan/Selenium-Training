516324c8f3378b7cfa38322b79b42f6809830547
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-13 10:12:15 -0500

    Date Picker

---
+++ Selenium-Essential-Training/src/test/java/interacting/DatePicker.java
@@ -0,0 +1,22 @@
+package interacting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DatePicker {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://formy-project.herokuapp.com/datepicker");
+
+        WebElement dateField = driver.findElement(By.id("datepicker"));
+        dateField.sendKeys("20/07/2025");
+        dateField.sendKeys(Keys.RETURN);
+        driver.quit();
+    }
+}
