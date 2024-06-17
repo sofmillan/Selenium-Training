7ef024f004327579395e37f72292a154a389e635
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-17 16:07:37 -0500

    Adding assertions

--- Selenium-Essential-Training/src/test/java/alltogether/Form.java
+++ Selenium-Essential-Training/src/test/java/alltogether/Form.java
@@ -3,8 +3,13 @@ package alltogether;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Form {
     public static void main(String[] args) throws InterruptedException {
@@ -22,6 +27,13 @@ public class Form {
         driver.findElement(By.id("datepicker")).sendKeys("05/20/2025");
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN); //close the datepicker
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+
+        String alertText = alert.getText();
+
+        assertEquals("The form was successfully submitted!", alertText);
 
         driver.quit();
     }
