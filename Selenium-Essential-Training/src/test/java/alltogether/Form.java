9b50c5aad2e6982bf241fd39fbc762c57f907e4d
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-17 16:13:09 -0500

    Cleaning up test

--- Selenium-Essential-Training/src/test/java/alltogether/Form.java
+++ Selenium-Essential-Training/src/test/java/alltogether/Form.java
@@ -18,6 +18,16 @@ public class Form {
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://formy-project.herokuapp.com/form");
 
+        submitForm(driver);
+
+        waitForAlertBanner(driver);
+
+        assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+
+        driver.quit();
+    }
+
+    public static void submitForm(WebDriver driver){
         driver.findElement(By.id("first-name")).sendKeys("Ricky");
         driver.findElement(By.id("last-name")).sendKeys("Shen");
         driver.findElement(By.id("job-title")).sendKeys("Singer");
@@ -27,14 +37,14 @@ public class Form {
         driver.findElement(By.id("datepicker")).sendKeys("05/20/2025");
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN); //close the datepicker
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+    }
 
+    public static void waitForAlertBanner(WebDriver driver){
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+    }
 
-        String alertText = alert.getText();
-
-        assertEquals("The form was successfully submitted!", alertText);
-
-        driver.quit();
+    public static String getAlertBannerText(WebDriver driver){
+        return driver.findElement(By.className("alert")).getText();
     }
 }
