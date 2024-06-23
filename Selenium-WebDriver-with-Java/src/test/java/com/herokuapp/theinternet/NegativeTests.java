25108d78aef13c988c85607010ce98477d0e9871
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-22 23:02:05 -0500

    Writing tests for incorrect user name

---
+++ Selenium-WebDriver-with-Java/src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -0,0 +1,39 @@
+package com.herokuapp.theinternet;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+    @Test
+    public void incorrectUserNameTest(){
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+
+        driver.manage().window().maximize();
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("ricky");
+
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
+
+
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.id("flash"));
+        String expectedMessage = "Your username is invalid!";
+        String actualMessage = errorMessage.getText();
+        assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain expected message");
+
+        driver.quit();
+    }
+}
