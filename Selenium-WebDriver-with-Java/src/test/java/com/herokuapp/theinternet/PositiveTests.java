fe4ee178b3d80a58b43a93443a78a30232888d6c
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-22 22:41:10 -0500

    Implementing assertions

--- Selenium-WebDriver-with-Java/src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ Selenium-WebDriver-with-Java/src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PositiveTests {
     @Test
     public void loginTest() throws InterruptedException {
@@ -17,10 +20,31 @@ public class PositiveTests {
         driver.manage().window().maximize();
 
         WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+
         WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
+
+
         WebElement loginButton = driver.findElement(By.tagName("button"));
+        loginButton.click();
+
+
+        String expectedUrl = "https://the-internet.herokuapp.com/secure";
+        String actualUrl = driver.getCurrentUrl();
+        assertEquals(expectedUrl, actualUrl, "Actual page url is not the same as expected");
+
+
         WebElement logoutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
-        WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
+        assertTrue(logoutButton.isDisplayed());
+
+        WebElement successMessage = driver.findElement(By.className("success"));
+        String expectedMessage = "You logged into a secure area!";
+        String actualMessage = successMessage.getText();
+        assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain expected message");
+
+
         driver.quit();
     }
 }
