e2df37f5e8386138cfc5ff46a07dcac96eabeef0
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-22 23:04:01 -0500

    Tests for incorrect password

--- Selenium-WebDriver-with-Java/src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ Selenium-WebDriver-with-Java/src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -36,4 +36,32 @@ public class NegativeTests {
 
         driver.quit();
     }
+
+    @Test
+    public void incorrectPasswordTest(){
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+
+        driver.manage().window().maximize();
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("gguggugi!");
+
+
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.id("flash"));
+        String expectedMessage = "Your password is invalid!";
+        String actualMessage = errorMessage.getText();
+        assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain expected message");
+
+        driver.quit();
+    }
 }
